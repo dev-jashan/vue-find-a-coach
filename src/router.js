3fd3dc44d041b0b249838119744ec51cd6b6a77f
Dev-JashanMadan <75950448+dev-jashan@users.noreply.github.com>
2022-02-02 04:02:26 -0500

    vuex and router is setup

---
+++ src/router.js
@@ -0,0 +1,25 @@
+import {createRouter,createWebHistory} from 'vue-router';
+
+import CoachesDetails from './pages/coaches/CoachesDetails.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachesRegisteration from './pages/coaches/CoachesRegisteration.vue';
+import ContactCoach from './pages/requests/ContachCoach.vue';
+import RequestRegisteration from './pages/coaches/CoachesRegisteration.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router= createRouter({
+
+    history:createWebHistory(),
+    routes:[
+        {path:'/', redirect:'/coaches'},
+        {path:'/coaches', component:CoachesList},
+        {path:'/coaches:id', component:CoachesDetails,
+            children:[
+                {path:'contact', component:ContactCoach},
+            ]},
+        {path:'/register', component:CoachesRegisteration},
+        {path:'/requests', component:RequestRegisteration},
+        {path:'/:notFound(.*)', component:NotFound}
+        
+    ]
+})
